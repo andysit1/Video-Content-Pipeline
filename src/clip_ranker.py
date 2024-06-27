20770e9699db31cac59ae53278b739dd45250bd3
Andy Sit <andysit173@gmail.com>
2024-06-27 01:01:54 -0400

    added points and weight

--- src/clip_ranker.py
+++ src/clip_ranker.py
@@ -1,7 +1,9 @@
 import cv2
 from base.clip_object import Clip
-from frame_extraction.main import crop_viewable_region, save_out_frames
-from frame_extraction.utils import average_method, luminosity_method
+
+from phase_two_selection.points import percentage_of_white_pixels
+from frame_extraction.main import crop_viewable_region, crop_image_crosshair, save_out_frames
+from frame_extraction.utils import average_method, luminosity_method, combine_output_frames_into_video
 import numpy as np
 #taks a clip objet and starts processing the rank
 from sklearn.cluster import KMeans
@@ -20,7 +22,6 @@ class Ranker:
 
     self.processed_frames_color = []
 
-
     self.processed_frames_color_threshold = []
     self.processed_frames_color_dom = []
 
@@ -28,6 +29,12 @@ class Ranker:
     self.constant_fps = None
     self.constant_frames = None
 
+
+    self.action_weight : int = 1
+    self.action_points : int = 0
+
+
+  #this should be in the clip object
   def load_frame(self, frame_path : str):
     if os.path.exists(path=frame_path):
       print("Successfully Loaded {}".format(frame_path))
@@ -36,8 +43,24 @@ class Ranker:
     else:
       print("Frame not found at {}".format(frame_path))
 
+  def clear(self) -> None:
+    self.frames = []
+    self.processed_frames = []
+    self.processed_frames_edge = []
+    self.processed_frames_threshold = []
+
+    self.processed_frames_color = []
+
+    self.processed_frames_color_threshold = []
+    self.processed_frames_color_dom = []
+
+    self.action_weight : int = 1
+    self.action_points : int = 0
+
   #will split the video into frames every .5 second assuming a 60 fps video
   def load_clip(self, clip : Clip):
+    self.clear()
+
     cap = cv2.VideoCapture(clip.get_path())
     self.constant_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
     self.constant_fps = cap.get(cv2.CAP_PROP_FPS)
@@ -56,12 +79,15 @@ class Ranker:
       c += 1
 
   def simplify_frames(self):
-    region =  np.array([[100, 100],[1600, 100],[1600,800],[100, 800]], np.int32)
+    # region =  np.array([[100, 100],[1600, 100],[1600,800],[100, 800]], np.int32)
 
     for frame in self.frames:
-      processedImage = crop_viewable_region(frame, [region])
+      # processedImage = crop_viewable_region(img=frame, region=[region])
+      processedImage = crop_image_crosshair(img=frame)
       processedImage = cv2.cvtColor(processedImage, cv2.COLOR_BGR2GRAY)
-      processedImage = cv2.GaussianBlur(processedImage,(5,5),0)
+
+      #Note, its better to have a lower blur inorder to see more details, at 11,11 we loss orb details and it starts not noticing players
+      processedImage = cv2.GaussianBlur(processedImage, (5, 5),0)
 
       #after applying and removing excess screen we can begin processing the frames
       self.processed_frames.append(processedImage)
@@ -95,6 +121,9 @@ class Ranker:
 
 
 
+
+
+  #if we calculate the dom colors within the range of the screen, we can determine when we see players or is looking at interesting objects
   def color_dom_processing(self, frame):
     small_frame = cv2.resize(frame, (0, 0), fx=0.1, fy=0.1)
     # Reshape the image to be a list of pixels
@@ -137,11 +166,7 @@ class Ranker:
 
   def color_blacknwhite_threshold_processing(self):
     for frame in self.processed_frames:
-
-
-
       dom_colors = self.color_dom_processing(frame=frame)
-      print("Domaniant colors for processed images", dom_colors[0], dom_colors[-1])
       print(average_method(dom_colors[0]), luminosity_method(dom_colors[0]))
 
       _, thresh = cv2.threshold(frame, luminosity_method(dom_colors[0]), luminosity_method(dom_colors[1]), cv2.THRESH_BINARY)
@@ -155,15 +180,26 @@ class Ranker:
       edges = cv2.Canny(frame, 100, 200)
       self.processed_frames_edge.append(edges)
 
+
+  #after experimentating 200 is a pretty good threshold since it cuts out a lot noise and effects
+  # @ 170 we still see effects such as brim smokes, likely need to tune per game but we will focus on Valorant for now
   def threshold(self):
     for frame in self.processed_frames:
-        _, thresh = cv2.threshold(frame, 100, 255, cv2.THRESH_BINARY)
+        _, thresh = cv2.threshold(frame, 170, 255, cv2.THRESH_BINARY)
         self.processed_frames_threshold.append(thresh)
 
+        #in the future we can change the action weight var to change the type of video we produce...
+        self.action_points += percentage_of_white_pixels(thresh) * self.action_weight
 
   def run(self):
     self.simplify_frames()
     self.threshold()
+
+
+  def get_points(self):
+    return self.action_points
+
+
     # self.color_blacknwhite_threshold_processing()
     # Add more processing steps as needed
 
@@ -181,18 +217,21 @@ if __name__ == "__main__":
 
 
   #Testing Frame
-
-  frame_path = "./frame_extraction/in_frame/demo2.png"
-  frame_path1 = "./frame_extraction/in_frame/demo3.jpg"
-
-  clip_ranker.load_frame(frame_path=frame_path)
-  clip_ranker.load_frame(frame_path=frame_path1)
+  # frame_path = "./frame_extraction/in_frame/demo2.png"
+  # frame_path1 ="./frame_extraction/in_frame/demo3.jpg"
+  # clip_ranker.load_frame(frame_path=frame_path)
+  # clip_ranker.load_frame(frame_path=frame_path1)
 
 
+  #video 78 = 3386.9422222222233
+  #video 25 = 2291.168888888889
+  #video 54 = 114.755
+
+  video_file = "../output-video/video015.mp4"
+  clip1 = Clip(path=video_file)
+  clip_ranker.load_clip(clip1)
   clip_ranker.run()
 
-  # save_out_frames(clip_ranker.frames, "frames")
-  save_out_frames(clip_ranker.processed_frames, "process_frames")
   save_out_frames(clip_ranker.processed_frames_threshold, "process_frames_threshold")
-
+  combine_output_frames_into_video()
 
