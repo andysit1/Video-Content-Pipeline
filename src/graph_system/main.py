2148b9e3e7c9f946c4238c399709bfe6d9a2c309
Andy Sit <andysit173@gmail.com>
2024-06-27 02:30:12 -0400

    graph outline

---
+++ src/graph_system/main.py
@@ -0,0 +1,15 @@
+"""
+BRAIN STORM
+
+In this folder system we want to start to figure out the different interactions we have in our graph
+from ending action to action, none action to action and vice versa, also important to figure out what type of videos
+we want to automate. For example, of we want summaries we can focus on giving more weight to equal timming between clips,
+or if we want to give clip that flow nicely together clips that have similar area of focus compared to other
+
+Steps:
+First first out how to implement a graph structure that supports time and action
+
+Second figure out more creative ways to get data from video processing and a way to effect the weighing of these factors
+
+Third we need to refactor a lot of the code to work better
+"""
