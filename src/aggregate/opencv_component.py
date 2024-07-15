5b0292d2196c4abf0fe7d44a7789f023a06a0ace
Andy Sit <andysit173@gmail.com>
2024-07-15 12:04:23 -0400

    updated opencv aggregate

--- src/aggregate/opencv_component.py
+++ src/aggregate/opencv_component.py
@@ -8,88 +8,75 @@ Handles the OpenCV/Numpy aspect of the code
 import cv2
 import numpy as np
 import glob
+import numpy as np
 import os
 
+from utils import percentage_of_white_pixels
+
+
 class OpenCVAggregate:
+    def __init__(self):
+        self.crosshair_offset = 75   #distance from center to left, right, top, down
+        self.gaus_blue_strength : tuple = (5, 5)
 
-    def crop_viewable_region(self, img, region=None):
-        self.logger.debug(f"Cropping viewable region with input: {img}, region: {region}")
-        mask = np.zeros_like(img)
-        cv2.fillPoly(mask, region, (255, 255, 255))
-        masked = cv2.bitwise_and(img, mask)
-        self.logger.debug(f"Masking output: {masked}")
-        return masked
+    def is_gray(self, img) -> bool:
+        if len(img.shape) != 2:
+            return False
+        return True
 
-    def crop_image_crosshair(self, img):
-        self.logger.debug("Cropping image around crosshair")
-        shape = img.shape
-        x_mid = int(shape[0] / 2)
-        y_mid = int(shape[1] / 2)
-        offset = 75
-        cropped_img = img[x_mid-offset:x_mid+offset , y_mid-offset:y_mid+offset]
-        self.logger.debug(f"Cropped image: {cropped_img}")
-        return cropped_img
+    #given a set of points  example region =  np.array([[100, 100],[1600, 100],[1600,800],[100, 800]], np.int32)
+    def crop_viewable_region(self, img : np.ndarray, region=None) -> np.ndarray:
+        if region:
+            mask = np.zeros_like(img)
+            cv2.fillPoly(mask, region, (255, 255, 255))
+            masked = cv2.bitwise_and(img, mask)
+            return masked
+        return False
+
+    #gets center then offets to get viewable region of the crosshair
+    def crop_image_crosshair(self, img : np.ndarray) -> np.ndarray:
+        height, width = img.shape[:2]
+        if width > self.crosshair_offset and height > self.crosshair_offset:
+            x_mid, y_mid = int(width[0] / 2), int(height[1] / 2)
+            offset = self.crosshair_offset
+            cropped_img = img[x_mid-offset:x_mid+offset , y_mid-offset:y_mid+offset]
+            return cropped_img
+        return False
+
+    def change_color_to_BRG2GRAY(self, img : np.ndarray) -> np.ndarray:
+        return cv2.cvtColor(
+            img,
+            cv2.COLOR_BGR2GRAY
+        )
+
+    def do_gaussian_blur(self, img : np.ndarray) -> np.ndarray:
+        return cv2.GaussianBlur(
+            img,
+            self.gaus_blue_strength,
+            0
+        )
+
+    def do_canny_edge_detection(self, img : np.ndarray) -> np.ndarray:
+        return cv2.Canny(
+            img,
+            threshold1 = 200, #TODO/CHANGE make these values init vars
+            threshold2=300
+        )
+
+    def get_gaussian_gaussian_blur_white_percentage(self, img : np.ndarray) -> float:
+        if self.is_gray(img):
+           return percentage_of_white_pixels(self.do_gaussian_blur(img))
+
+        gray_img = self.change_color_to_BRG2GRAY(img)
+        return percentage_of_white_pixels(self.do_gaussian_blur(gray_img))
+
+    def get_canny_edge_detection_white_percentage(self, img) -> float:
+        if self.is_gray(img):
+           return percentage_of_white_pixels(self.do_canny_edge_detection(img))
+
+        gray_img = self.change_color_to_BRG2GRAY(img)
+        return percentage_of_white_pixels(self.do_canny_edge_detection(gray_img))
 
 
-    def get_filed_info(self) -> list:
-        files = self.load_frames()
-        info = []
 
-        for file in files:
-            image = cv2.imread(file)
-            processedImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            processedImage = cv2.Canny(processedImage, threshold1 = 200, threshold2=300)
-            processedImage = cv2.GaussianBlur(processedImage,(5,5),0)
-            info.append(processedImage)
-            self.logger.debug(f"Processed image: {processedImage}")
 
-        return info
-
-    def get_cropped(self):
-        self.logger.debug("Starting to crop images")
-        data = self.get_filed_info()
-        region =  np.array([[100, 100],[1600, 100],[1600,800],[100, 800]], np.int32)
-        cropped = []
-        self.logger.debug(f"Processed data for cropping: {data}")
-
-        for arr in data:
-            img = self.crop_viewable_region(arr, region=[region])
-            self.logger.debug(f"Cropped image: {img}")
-            cropped.append(img)
-
-        self.logger.debug(f"All cropped images: {cropped}")
-        return cropped
-
-    def save_out_frames(self, images, pattern: str):
-        self.logger.debug("Saving out frames")
-        current_dir = os.path.dirname(os.path.dirname(__file__))
-        c = 0
-
-        self.remove_all_contents_output_frame()
-        for image in images:
-            out_file = os.path.join(current_dir, "frame_extraction", "out_frame", f"{pattern}{c}.png")
-            if not cv2.imwrite(out_file, image):
-                raise Exception("Could not write image")
-            self.logger.debug(f"Saved image: {out_file}")
-            c += 1
-
-    def process_frames(self):
-        self.logger.debug("Processing frames")
-        files = self.load_frames()
-        current_dir = os.path.dirname(os.path.dirname(__file__))
-        c = 0
-
-        for file in files:
-            image = cv2.imread(file)
-            processedImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            processedImage = cv2.Canny(processedImage, threshold1 = 200, threshold2=300)
-            processedImage = cv2.GaussianBlur(processedImage,(5,5),0)
-            out_file = os.path.join(current_dir, "frame_extraction", "out_frame", f"demo_gaus{c}.png")
-
-            cv2.imshow("Processed", processedImage)
-            self.logger.debug(f"Processed image: {processedImage}")
-            self.logger.debug(f"Output file: {out_file}")
-
-            if not cv2.imwrite(out_file, processedImage):
-                raise Exception("Could not write image")
-            c += 1
