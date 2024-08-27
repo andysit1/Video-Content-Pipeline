e4af0231cdf9a0346932e59e6a06a605d1c4a3fb
Andy Sit <andysit173@gmail.com>
2024-08-27 00:56:24 -0400

    swaped out the videostream class for opencv

--- src/pipes/analyze_clip_stage.py
+++ src/pipes/analyze_clip_stage.py
@@ -7,7 +7,7 @@ import os
 from icecream import ic
 from src.modules.pipeline_builder import Pipe
 from src.modules.video_stream import VideoStream
-from src.aggregate.opencv_component import OpenCVAggregate
+from src.aggregate.opencv_component import OpenCVAggregate, cv2 as cv
 from src.aggregate.filehandler_component import FileHandleComponent
 import glob
 import cv2
@@ -19,6 +19,7 @@ logger = logging.getLogger(__name__)
 class AnalyzeClipsPipe(Pipe, OpenCVAggregate, FileHandleComponent):
   def __init__(self, engine):
     super().__init__(engine)
+    self.crosshair_offset = 75  # Initialize the attribute here
 
     self.low_piority_weight : int = 1
     self.analyze_data = os.path.join(self.engine.payload['cache_txt_out'], "analyze_data.txt")
@@ -40,7 +41,14 @@ class AnalyzeClipsPipe(Pipe, OpenCVAggregate, FileHandleComponent):
     return sorted(glob.glob(os.path.join(self.engine.payload['clips_out'], "*")), key=os.path.getmtime)
 
   def on_done(self):
-    self.engine.machine.next_state = CompileVideoPipe(self.engine)
+    #compile -> make into video
+    #ELSE
+    #end program
+    if self.engine.compile:
+      self.engine.machine.next_state = CompileVideoPipe(self.engine)
+    else:
+      self.engine.machine.next_state = None
+
 
   def is_analyze_cache(self):
     if self.file_exists(self.analyze_data):
@@ -48,9 +56,7 @@ class AnalyzeClipsPipe(Pipe, OpenCVAggregate, FileHandleComponent):
     return False
 
   def cache_analyze_data(self):
-    time = self.get_duration_data()
-    data = zip(self.score, time)
-    self.write_lines(path=self.analyze_data, lines=data)
+    self.write_lines(path=self.analyze_data, lines=self.score)
 
   def sort_video_order(self):
     di = sorted(self.score, key="points")
@@ -62,59 +68,121 @@ class AnalyzeClipsPipe(Pipe, OpenCVAggregate, FileHandleComponent):
     regions of the screen
   """
 
-
-
-  #add focus point as a stat
   def on_run(self):
+
     if not self.is_analyze_cache():
       ic.enable()
-      ic("Analyzing Clips....")
       clips = self.get_clips()
-      cv_color = cv2.COLOR_YUV2RGB_I420       # OpenCV converts YUV frames to RGB
 
       for clip in clips:
+        #per clip vars
         gaus_white_percentage = 0
         canny_white_percentage = 0
-        if os.path.exists(clip):
-          try:
-            video = VideoStream(path=clip) #load yuv by default
-            video.open_stream()
 
-            frames = 0
-            ic()
-            while True:
-              eof, frame = video.read()
-              if eof:
-                ic("closing...")
-                video.close()
+        cap = cv.VideoCapture(os.path.abspath(clip))
+        frame_count = 0
+        while cap.isOpened():
+            frame_count += 1
+            ret, frame = cap.read()
+
+            if not frame_count % 30:
+                continue
+
+            # if frame is read correctly ret is True
+            if not ret:
+                cap.release()
                 break
-              frames += 1
-
-              if (frames % 30) == 1:
-                ic(frames)
-                arr = np.frombuffer(frame, np.uint8).reshape(video.shape()[1] * 3 // 2, video.shape()[0]) #Why does this work
-
-                if self.crop_image_crosshair(arr):
-                  gaus_white_percentage += self.do_binary_threshold(img=arr)
-                  canny_white_percentage += self.get_canny_edge_detection_white_percentage(img=arr)
-                # self.get_focus_point(arr)
 
 
-            data_obj = {
-              'name' : clip,
-              'gaus_white_percentage' : gaus_white_percentage,
-              'canny_white_percentage': canny_white_percentage,
-              'points' : gaus_white_percentage * 2 + canny_white_percentage * 0.5,
-              'focus' : "3,4"
-            }
+            if len(frame.shape) == 2:  # Single channel image
+                gray_frame = frame  # Already grayscale
+            else:
+                gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-            self.score.append(data_obj)
-          except:
-            pass
-      else:
-        ic("Clip does not exist")
-      self.cache_analyze_data() #cache the data we got from processing
+            try:
+              crosshair = self.crop_image_crosshair(gray_frame)
+              ic(crosshair)
+            except Exception as e:
+              ic(e)
+            else:
+              gaus_white_percentage += self.do_binary_threshold(img=crosshair)
+              canny_white_percentage += self.get_canny_edge_detection_white_percentage(img=crosshair)
+              # ic(canny_white_percentage)
+              # focus = self.get_focus_point(gray_frame)
+
+              #add a new data row entry for the stats picked up
+              data_obj = {
+                'name' : os.path.abspath(clip),
+                'gaus_white_percentage' : gaus_white_percentage,
+                'canny_white_percentage': canny_white_percentage,
+                'points' : gaus_white_percentage * 2 + canny_white_percentage * 0.5,
+              }
+
+        self.score.append(data_obj)
+      self.cache_analyze_data()
 
     self.on_done()
+
+
+
+
+"""
+  Video stream class is not working, going to switch back to opencv
+"""
+
+
+  # #add focus point as a stat
+  # def on_run(self):
+  #   if not self.is_analyze_cache():
+  #     ic.enable()
+  #     ic("Analyzing Clips....")
+  #     clips = self.get_clips()
+
+  #     for clip in clips:
+  #       gaus_white_percentage = 0
+  #       canny_white_percentage = 0
+
+  #       if os.path.exists(clip):
+  #         try:
+  #           video = VideoStream(path=clip) #load yuv by default
+  #           video.open_stream()
+
+  #           frames = 0
+  #           ic()
+  #           while True:
+  #             eof, frame = video.read()
+  #             ic("erroring...")
+
+  #             if eof:
+  #               ic("closing...")
+  #               video.close()
+  #               break
+  #             frames += 1
+
+  #             if (frames % 30) == 1:
+  #               ic(frames)
+  #               arr = np.frombuffer(frame, np.uint8).reshape(video.shape()[1] * 3 // 2, video.shape()[0]) #Why does this work
+
+  #               if self.crop_image_crosshair(arr):
+  #                 gaus_white_percentage += self.do_binary_threshold(img=arr)
+  #                 canny_white_percentage += self.get_canny_edge_detection_white_percentage(img=arr)
+  #               # self.get_focus_point(arr)
+
+
+  #           data_obj = {
+  #             'name' : os.path.abspath(clip),
+  #             'gaus_white_percentage' : gaus_white_percentage,
+  #             'canny_white_percentage': canny_white_percentage,
+  #             'points' : gaus_white_percentage * 2 + canny_white_percentage * 0.5,
+  #           }
+
+  #           self.score.append(data_obj)
+  #         except:
+  #           pass
+  #     else:
+  #       ic("Clip does not exist")
+  #     self.cache_analyze_data() #cache the data we got from processing
+
+  #   self.on_done()
 
 
